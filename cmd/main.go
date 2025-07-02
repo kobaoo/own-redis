f32e5de0c2b4513c610eeb8eeef6aef8c1329eb3
kobaoo <koblandikz@gmail.com>
2025-07-02 16:21:10 +0500

    idk moment

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
+	"net"
 	"own-redis/internal/config"
 )
 
@@ -10,4 +13,18 @@ func main() {
 	if err != nil {
 		log.Fatal("Failed during flag parsing", err)
 	}
+
+	p := make([]byte, 2048)
+	conn, err := net.Dial("upd", fmt.Sprintf("127.0.0.1:%d", config.Port))
+	if err != nil {
+		log.Fatal("Failed to connect with udp")
+	}
+	fmt.Fprintf(conn, "HELLOO IT WORKS")
+	_, err = bufio.NewReader(conn).Read(p)
+	if err == nil {
+		fmt.Printf("%s\n", p)
+	} else {
+		fmt.Printf("Some error %v\n", err)
+	}
+	conn.Close()
 }
